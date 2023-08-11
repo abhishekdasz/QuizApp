043bde76ce36a4e1bcdff7ff762dc498971286e8
Abhishek <abhishekdaszy@gmail.com>
2023-08-12 02:06:42 +0530

    added checkbox option & update frontend

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,39 +1,64 @@
 import React, { useState } from 'react'
 import { QuizData } from '../constants/QuizData';
+import '../App.css'
 
 const Quiz = () => {
     const [currQueIndex, setCurrQueIndex] = useState(0);
     const currentQue = QuizData[currQueIndex];
-    const [selectedOptionIndex, setSelectedOptionIndex] = useState(null);
+    const [selectedOptions, setSelectedOptions] = useState([]);
     const [score, setScore] = useState(0);
     const [submitted, setSubmitted] = useState(false);
 
+    const avg = (score/QuizData.length)*100;
+
+    const isOptionSelected = (option) => {
+        return selectedOptions.some((selectedOption) => selectedOption.id === option.id);
+    }
+
     const handleOptionChange = (optIndex) =>{
-        setSelectedOptionIndex(optIndex);
+        const option = currentQue.options[optIndex];
+        setSelectedOptions((prevSelectedOptions) => {
+            if (isOptionSelected(option)) 
+            {
+              return prevSelectedOptions.filter((selectedOption) => selectedOption.id !== option.id);
+            } 
+            else 
+            {
+              return [...prevSelectedOptions, option];
+            }
+        })
+        // setSelectedOptionIndex(optIndex);
     }
     const handleNextQue = () => {
-        if(selectedOptionIndex!==null)
+        if (selectedOptions.length === 0) {
+            alert("Please select at least one option before proceeding.");
+            return;
+        }
+        if (selectedOptions.length > 0) 
         {
-            const selectOption = currentQue.options[selectedOptionIndex];
-            if(selectOption.isCorrect)
-            {
-                setScore(score+1);
+            const question = QuizData[currQueIndex];
+            const allSelectedAreCorrect = selectedOptions.every((option) => option.isCorrect);
+            const allCorrectOptionsSelected = selectedOptions.length === question.options.filter(opt => opt.isCorrect).length;
+      
+            if (allSelectedAreCorrect && allCorrectOptionsSelected) {
+                setScore(score + 1);
             }
-            if(currQueIndex < QuizData.length-1)
-            {
-                setCurrQueIndex(currQueIndex+1);
-                setSelectedOptionIndex(null);
+      
+            if (currQueIndex < QuizData.length - 1) {
+                setCurrQueIndex(currQueIndex + 1);
+                setSelectedOptions([]);
             }
         }
     }
     const handleSubmitQuiz = () =>{
-        if(selectedOptionIndex !== null)
+        if (selectedOptions.length > 0) 
         {
-            const selectOption = currentQue.options[selectedOptionIndex];
-            if(selectOption.isCorrect)
+            const allCorrect = selectedOptions.every((option) => option.isCorrect);
+      
+            if (allCorrect) 
             {
-                setScore(score+1);
-            } 
+              setScore(score + 1);
+            }
         }
         setSubmitted(true);
     }
@@ -42,7 +67,10 @@ const Quiz = () => {
     {
         return(
             <div className='quiz-sec'>
-                <p> Final Score: {score} </p>
+                <div className="score">
+                    <p> Final Score: {score}/{QuizData.length} </p> <br />
+                    <p> Avg is: {avg}%  </p>
+                </div>
             </div>
         )
     }
@@ -52,21 +80,22 @@ const Quiz = () => {
   return (
     <div className='quiz-sec'>
         <div className="quiz-container">
+            <div className='instructions'> 
+                <p> Instruction: {currentQue.view ==='checkbox-opt' ? <> You can select multiple options </> : <> Choose the correct option only </>}  </p> 
+            </div>
             <h3> {currentQue.question} </h3>
-            {
-                currentQue.options.map((opt, index)=>(
-                    <label>
-                        <input 
-                            type="radio"
-                            name='answer'
-                            value={opt.id}
-                            checked={index === selectedOptionIndex} 
-                            onChange={()=>handleOptionChange(index)}
-                        />
-                        {opt.label}
-                    </label>
-                ))
-            }
+            {currentQue.options.map((opt, index) => (
+          <label key={opt.id}>
+            <input
+              type={currentQue.view === 'radio-opt' ? 'radio' : 'checkbox'}
+              name='answer'
+              value={opt.id}
+              checked={isOptionSelected(opt)}
+              onChange={() => handleOptionChange(index)}
+            />
+            {opt.label}
+          </label>
+        ))}
             {
                 currQueIndex < QuizData.length-1 ? 
                 ( <button onClick={handleNextQue}> Next </button> ) :
--- src/constants/QuizData.js
+++ src/constants/QuizData.js
@@ -1,30 +1,112 @@
 export const QuizData = [
     {
-        id:1,
-        question: 'Which color do you prefer?',
-        options: 
-        [
-            { id: 11, label: 'Red', isCorrect: false },
-            { id: 12, label: 'Blue', isCorrect: true },
-            { id: 13, label: 'Green', isCorrect: false },
+        id: 1,
+        question: 'Which of the following are front-end programming languages?',
+        options: [
+            { id: 11, label: 'HTML', isCorrect: true },
+            { id: 12, label: 'Python', isCorrect: false },
+            { id: 13, label: 'Java', isCorrect: false },
+            { id: 14, label: 'CSS', isCorrect: true },
         ],
+        view: 'checkbox-opt'
     },
     {
         id: 2,
-        question: 'Select a fruit:',
+        question: 'Which languages can be used for server-side programming?',
         options: [
-          { id: 21, label: 'Apple', isCorrect: true },
-          { id: 22, label: 'Banana', isCorrect: false },
-          { id: 23, label: 'Orange', isCorrect: false },
+            { id: 21, label: 'JavaScript', isCorrect: true },
+            { id: 22, label: 'PHP', isCorrect: true },
+            { id: 23, label: 'Ruby', isCorrect: true },
+            { id: 24, label: 'HTML', isCorrect: false },
         ],
+        view: 'checkbox-opt'
     },
     {
         id: 3,
-        question: 'Select a season:',
+        question: 'Which tool can be used for version control in web development?',
         options: [
-          { id: 31, label: 'Summer', isCorrect: true },
-          { id: 32, label: 'Rain', isCorrect: false },
-          { id: 33, label: 'Winter', isCorrect: false },
+            { id: 31, label: 'Git', isCorrect: true },
+            { id: 32, label: 'Subversion', isCorrect: false },
+            { id: 33, label: 'Mercurial', isCorrect: false },
+            { id: 34, label: 'SVN', isCorrect: false },
         ],
+        view: 'radio-opt'
     },
-]
+    {
+        id: 4,
+        question: 'Which of the following is a popular front-end framework/library?',
+        options: [
+            { id: 41, label: 'Angular', isCorrect: true },
+            { id: 42, label: 'Node.js', isCorrect: false },
+            { id: 43, label: 'Express', isCorrect: false },
+            { id: 44, label: 'Django', isCorrect: false },
+        ],
+        view: 'radio-opt'
+    },
+    {
+        id: 5,
+        question: 'What does CSS stand for?',
+        options: [
+            { id: 51, label: 'Cascading Style Sheet', isCorrect: true },
+            { id: 52, label: 'Computer Style Sheet', isCorrect: false },
+            { id: 53, label: 'Creative Style Sheet', isCorrect: false },
+            { id: 54, label: 'Colorful Style Sheet', isCorrect: false },
+        ],
+        view: 'radio-opt'
+    },
+    // {
+    //     id: 6,
+    //     question: 'Which HTTP method is used to retrieve data from a server?',
+    //     options: [
+    //         { id: 61, label: 'GET', isCorrect: true },
+    //         { id: 62, label: 'POST', isCorrect: false },
+    //         { id: 63, label: 'PUT', isCorrect: false },
+    //         { id: 64, label: 'DELETE', isCorrect: false },
+    //     ],
+    //     view: 'radio-opt'
+    // },
+    // {
+    //     id: 7,
+    //     question: 'What is the purpose of a media query in responsive web design?',
+    //     options: [
+    //         { id: 71, label: 'To apply styles based on the device screen size', isCorrect: true },
+    //         { id: 72, label: 'To include images and videos in the webpage', isCorrect: false },
+    //         { id: 73, label: 'To manage server-side requests', isCorrect: false },
+    //         { id: 74, label: 'To handle form submissions', isCorrect: false },
+    //     ],
+    //     view: 'radio-opt'
+    // },
+    // {
+    //     id: 8,
+    //     question: 'Which of the following is a version of HTML?',
+    //     options: [
+    //         { id: 81, label: 'HTML5', isCorrect: true },
+    //         { id: 82, label: 'XML', isCorrect: false },
+    //         { id: 83, label: 'SGML', isCorrect: false },
+    //         { id: 84, label: 'XHTML', isCorrect: false },
+    //     ],
+    //     view: 'radio-opt'
+    // },
+    // {
+    //     id: 9,
+    //     question: 'What is the purpose of a JavaScript framework like React?',
+    //     options: [
+    //         { id: 91, label: 'To build user interfaces for web applications', isCorrect: true },
+    //         { id: 92, label: 'To perform server-side scripting', isCorrect: false },
+    //         { id: 93, label: 'To handle database operations', isCorrect: false },
+    //         { id: 94, label: 'To create RESTful APIs', isCorrect: false },
+    //     ],
+    //     view: 'radio-opt'
+    // },
+    // {
+    //     id: 10,
+    //     question: 'What is the primary function of a back-end server?',
+    //     options: [
+    //         { id: 101, label: 'To handle business logic and database operations', isCorrect: true },
+    //         { id: 102, label: 'To style and design web pages', isCorrect: false },
+    //         { id: 103, label: 'To provide a user-friendly interface', isCorrect: false },
+    //         { id: 104, label: 'To optimize front-end performance', isCorrect: false },
+    //     ],
+    //     view: 'radio-opt'
+    // },
+];
